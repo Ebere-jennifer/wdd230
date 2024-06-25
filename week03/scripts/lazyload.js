bdafb240358a6614878488395ed152f725bec11f
Ebere-jennifer <ebe22004@byui.edu>
2024-06-25 01:50:43 +0100

    Lazy loading (week 3 learning activities)

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const currentYear = new Date().getFullYear();
+  const currentYearElement = document.getElementById("currentYear");
+  currentYearElement.textContent = currentYear;
+
+  const lastModified = document.lastModified;
+  const lastModifiedElement = document.getElementById("lastModified");
+  lastModifiedElement.textContent = `Last Modification: ${lastModified}`;
+
+});
